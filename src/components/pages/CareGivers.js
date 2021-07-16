67335dfafb9524749214226b75f37c826997724d
Chris Germishuys <chris.germishuys@gmail.com>
2021-07-16 18:18:41 -0400

    Makes changes to sidenav and app.js to have sidenav and cssbaseline only load once

--- src/components/pages/CareGivers.js
+++ src/components/pages/CareGivers.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import { Row,Col} from 'reactstrap';
 import CU from './CU';
 import PwGen from '../pwgen/pwgen';
-import SideNav from '../shared/SideNav';
 
 export default function Caregivers() {
     return (
       <React.Fragment>
-          <SideNav/>
-        <CssBaseline />
         <Container fixed>
         <hr class="yellow"/>
             <Row className="my-5">
--- src/components/pages/Patients.js
+++ src/components/pages/Patients.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import { Row,Col} from 'reactstrap';
 import EnhancedTable from '../userlist/list';
-import SideNav from '../shared/SideNav';
 import Search from '../search';
 
 export default function Patients() {
     return (
       <React.Fragment>
-          <SideNav/>
-        <CssBaseline />
         <Container fixed>
         <hr class="yellow"/>
             <Row className="my-5">
