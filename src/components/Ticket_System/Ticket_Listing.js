ed301f2136912c33db61a80ab7d8ec2286b50d83
David Duchesneau-Hart <dduchesn@my.yorku.ca>
2021-07-19 21:53:11 -0400

    removed 2 unused var

--- src/components/Ticket_System/Ticket_Listing.js
+++ src/components/Ticket_System/Ticket_Listing.js
@@ -4,9 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import CardActions from '@material-ui/core/CardActions';
 import Bounce from 'react-reveal/Bounce';
 
 const useStyles = makeStyles({
