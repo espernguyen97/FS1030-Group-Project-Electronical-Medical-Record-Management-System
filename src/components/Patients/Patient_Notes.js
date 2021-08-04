f3a8fac887a61c6318cd0f7dc4235b32ec56e48f
Chris Germishuys <chris.germishuys@gmail.com>
2021-08-03 20:21:08 -0400

    Updated patient notes to only display specified id notes

--- src/components/Patients/Patient_Notes.js
+++ src/components/Patients/Patient_Notes.js
@@ -3,15 +3,19 @@ import parseJwt from "../../helpers/authHelper";
 import { Container } from "reactstrap";
 import { Table, Row } from "reactstrap";
 import moment from "moment";
+import { useLocation } from "react-router-dom";
 
 const Notes = () => {
   const token = sessionStorage.getItem("token");
   const user = parseJwt(token).username;
   const [notes, setnotes] = useState([]);
+  const location = useLocation();
+  const noteID = location.state.PatientID;
+
 
   useEffect(() => {
     const getData = async () => {
-      const response = await fetch("http://localhost:4000/notes/", {
+      const response = await fetch(`http://localhost:4000/notes/${noteID}`, {
         method: "GET",
         mode: 'cors',
         headers: {
