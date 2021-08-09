ef395fbfa870da7a02da89dfadc604bff9131dcd
Chris Germishuys <chris.germishuys@gmail.com>
2021-08-09 16:56:45 -0400

    Adds validation on front end for note for minimum of 20 characters

--- src/components/Patients/Create_Note.js
+++ src/components/Patients/Create_Note.js
@@ -1,150 +1,185 @@
-import React, { useEffect,useState } from 'react'
-import {Input, Form, FormGroup, Col, Button, Container} from 'reactstrap'
-import InputLabel from '@material-ui/core/InputLabel';
-import Tooltip from '@material-ui/core/Tooltip';
-import NoteAddIcon from '@material-ui/icons/NoteAdd';
+import React, { useEffect, useState } from "react";
+import { Input, Form, FormGroup, Col, Button, Container } from "reactstrap";
+import InputLabel from "@material-ui/core/InputLabel";
+import Tooltip from "@material-ui/core/Tooltip";
+import NoteAddIcon from "@material-ui/icons/NoteAdd";
 import parseJwt from "../../helpers/authHelper";
-import Swal from 'sweetalert2'
-import { useLocation } from 'react-router-dom';
-
-
+import Swal from "sweetalert2";
+import { useLocation } from "react-router-dom";
 
 const SQLDateParsed = () => {
-
-    // MySQL formatted UTC 
-    let d = new Date()
-    let SQLDate = new Date(
+  // MySQL formatted UTC
+  let d = new Date();
+  let SQLDate = new Date(
     d.getFullYear(),
     d.getMonth(),
     d.getDate(),
     d.getHours(),
-    (d.getMinutes()), 
+    d.getMinutes(),
     d.getSeconds(),
     d.getMilliseconds()
-    ).toISOString().slice(0, 19).replace('T', ' ')
-    return(SQLDate)
-}
-
-
+  )
+    .toISOString()
+    .slice(0, 19)
+    .replace("T", " ");
+  return SQLDate;
+};
 
 const PatientCreate = () => {
-    const location = useLocation();
-    const token = sessionStorage.getItem('token')
-    const [PatientID, setPatientID] = useState(location.state.PatientID)
-    const [Username, setUsername] = useState("")
-    const [Note, setNote] = useState("")
-    const [alertContent, setAlertContent] = useState(null)
-    const Date =  SQLDateParsed();
-    const user = parseJwt(token).userEmail;
-    const [patients, setPatients] = useState([]);
+  const location = useLocation();
+  const token = sessionStorage.getItem("token");
+  const [PatientID, setPatientID] = useState(location.state.PatientID);
+  const [Username, setUsername] = useState("");
+  const [Note, setNote] = useState("");
+  const [alertContent, setAlertContent] = useState(null);
+  const Date = SQLDateParsed();
+  const user = parseJwt(token).userEmail;
+  const [patients, setPatients] = useState([]);
 
-    useEffect(() => {
-        const getData = async () => {
-          const response = await fetch(`http://localhost:4000/users/${user}`, {
-            method: "GET",
-            mode: 'cors',
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          const data = await response.json();
-          setUsername(data.Username);
-        };
-        getData();
-      }, [token]);
-    
-  
-    useEffect(() => {
-      const getData = async () => {
-        const response = await fetch("http://localhost:4000/patients/", {
-          method: "GET",
-          mode: 'cors',
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`http://localhost:4000/users/${user}`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      setUsername(data.Username);
+    };
+    getData();
+  }, [token]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("http://localhost:4000/patients/", {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      setPatients(data);
+    };
+    getData();
+  }, [token]);
+
+  const formSubmit = async (event) => {
+    event.preventDefault();
+    if (Note.length > 20) {
+      const response = await fetch("http://localhost:4000/notes", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ Note, PatientID, Username, Date }),
+      });
+      const payload = await response.json();
+      if (response.status >= 400) {
+        setAlertContent(`Error with fields: ${payload.invalid.join(",")}`);
+      } else {
+        setAlertContent(null);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Success!",
+          titleText: "Success",
+          text: "A New Note Has been Created.",
+          confirmButtonColor: "#4BB543",
+          timer: 1500,
         });
-        const data = await response.json();
-        setPatients(data);
-      };
-      getData();
-    }, [token]);
-
-
-    const formSubmit = async (event) => {
-        event.preventDefault()
-        const response = await fetch('http://localhost:4000/notes', {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({Note, PatientID, Username,Date})
-        })
-        const payload = await response.json()
-        if (response.status >= 400) {
-            setAlertContent(`Error with fields: ${payload.invalid.join(",")}`)
-        } else {
-            setAlertContent(null)
-            Swal.fire({
-                position: 'top-end',
-                icon: 'success',
-                title: 'Success!',
-                titleText: 'Success' ,
-                text: 'A New Note Has been Created.',
-                confirmButtonColor: '#4BB543',
-                timer: 1500
-              })
-            resetForm()
-        }
+        resetForm();
+      }
+    } else {
+      setAlertContent(`Please enter a minimum of 20 characters for the note.`);
     }
+  };
 
-    const resetForm = () => {
-        setNote("")
-    }
-    return (
-        <main>
-            <Container className="containerPatient_Create">
-                <center>
-                <h2>Add Patient Note</h2>
-                <Form className="my-5" onSubmit={formSubmit}>
-                    <FormGroup>
-                        <Col>
-                           <InputLabel><b>PatientID</b></InputLabel>
-                            <Tooltip title="Patient ID will display here">
-                           <Input type="PatientID" name="PatientID" id="PatientID" placeholder="Enter The Patient ID" value={PatientID} disabled="true" />
-                            </Tooltip>
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                        <Col>
-                           <InputLabel><b>Username</b></InputLabel>
-                            <Tooltip title="Your Username will display here">
-                            <Input type="Username" name="Username" id="Username" placeholder="Select Your Username" value={Username} disabled="true" />                        
-                            </Tooltip>
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                        <Col>
-                           <InputLabel><b>Note</b></InputLabel>
-                            <Tooltip title="Enter The Note For the Patient">
-                            <Input type="textarea" name="Note" id="Note" placeholder="Note Here"  required value={Note} onChange={e => setNote(e.target.value) }/>
-                            </Tooltip>
-                        </Col>
-                    </FormGroup>
-                    <div className={`alert ${!alertContent ? "hidden" : ""}`}>{alertContent}</div>
-                    <FormGroup check row>
-                        <Col>
-                            <p style={{fontStyle: "italic"}}>Fill out all fields to add a patient note</p>
-                            <Button color="primary" type="submit"><NoteAddIcon/>Add Note</Button>
-                        </Col>
-                    </FormGroup>
-                </Form>
-          </center>
-            </Container>
-        </main>
-    )
-}
+  const resetForm = () => {
+    setNote("");
+  };
+  return (
+    <main>
+      <Container className="containerPatient_Create">
+        <center>
+          <h2>Add Patient Note</h2>
+          <Form className="my-5" onSubmit={formSubmit}>
+            <FormGroup>
+              <Col>
+                <InputLabel>
+                  <b>PatientID</b>
+                </InputLabel>
+                <Tooltip title="Patient ID will display here">
+                  <Input
+                    type="PatientID"
+                    name="PatientID"
+                    id="PatientID"
+                    placeholder="Enter The Patient ID"
+                    value={PatientID}
+                    disabled="true"
+                  />
+                </Tooltip>
+              </Col>
+            </FormGroup>
+            <FormGroup>
+              <Col>
+                <InputLabel>
+                  <b>Username</b>
+                </InputLabel>
+                <Tooltip title="Your Username will display here">
+                  <Input
+                    type="Username"
+                    name="Username"
+                    id="Username"
+                    placeholder="Select Your Username"
+                    value={Username}
+                    disabled="true"
+                  />
+                </Tooltip>
+              </Col>
+            </FormGroup>
+            <FormGroup>
+              <Col>
+                <InputLabel>
+                  <b>Note</b>
+                </InputLabel>
+                <Tooltip title="Enter The Note For the Patient">
+                  <Input
+                    type="textarea"
+                    name="Note"
+                    id="Note"
+                    placeholder="Note Here"
+                    required
+                    value={Note}
+                    onChange={(e) => setNote(e.target.value)}
+                  />
+                </Tooltip>
+              </Col>
+            </FormGroup>
+            <div className={`alert ${!alertContent ? "hidden" : ""}`}>
+              {alertContent}
+            </div>
+            <FormGroup check row>
+              <Col>
+                <p style={{ fontStyle: "italic" }}>
+                  Fill out all fields to add a patient note
+                </p>
+                <Button color="primary" type="submit">
+                  <NoteAddIcon />
+                  Add Note
+                </Button>
+              </Col>
+            </FormGroup>
+          </Form>
+        </center>
+      </Container>
+    </main>
+  );
+};
 
-export default PatientCreate
+export default PatientCreate;
